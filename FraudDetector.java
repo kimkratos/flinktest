f680c4a31e4acab21135f07d814c6d5d6139a8e1
kimkratos <tykratos@itcom888.com>
2024-07-01 14:40:03 +0800

    Update FraudDetector.java

--- FraudDetector.java
+++ FraudDetector.java
@@ -6,8 +6,8 @@ import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
-import org.apache.flink.walkthrough.common.entity.Alert;
-import org.apache.flink.walkthrough.common.entity.Transaction;
+import org.apache.flink.walkthrough.common.entity.Alert;   // 添加导入
+import org.apache.flink.walkthrough.common.entity.Transaction; // 添加导入
 
 public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
 
